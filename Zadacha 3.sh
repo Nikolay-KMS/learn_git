deabf2beef5b5872512166a975bb5043369cc001
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-11-01 07:32:32 +0200

    bach скрипт резервного копирования

--- Zadacha 3.sh
+++
@@ -1,6 +0,0 @@
-﻿#!/bin/bash
-
-
-
-tar -zcf /home/ubuntu/Desktop/Folder1/backups.tgz --exclude=*.dat /home/ubuntu/Desktop/Folder1/Folder2
-
---
+++ test4.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/bash
+
+tar -zcf /home/ubuntu/Desktop/Folder1/backups.tgz /home/ubuntu/Desktop/Folder1/Folder2
+
